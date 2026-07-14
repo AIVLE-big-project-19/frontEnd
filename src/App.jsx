0bf61b823662fbe7dc6b769c750d02883641c3b0
cxz2055-stack <cxz2055@gmail.com>
2026-07-14 17:47:17 +0900

    fix: 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./router/AppRouter";
-
-function App() {
-    return (
-        <BrowserRouter>
-            <AppRouter />
-        </BrowserRouter>
-    );
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
@@ -19,14 +7,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* 주소가 '/'일 때만 MainPage 표시 */}
         <Route path="/" element={<MainPage />} />
-        {/* 주소가 '/test'일 때만 TestPage 표시 */}
         <Route path="/test" element={<TestPage />} />
       </Routes>
     </Router>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
