205e7260976aa9945512f3f4c6b2bbcbc120a3a1
s2ungyeon-h <s2ungyeon.h@gamil.com>
2026-07-28 13:14:40 +0900

    feat: 추천 업로드/조회 API 모듈 추가

---
+++ src/api/recommendationApi.js
@@ -0,0 +1,18 @@
+import instance from './axiosInstance';
+
+export const uploadRecommendation = async (file, limit) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  const { data } = await instance.post('/recommendations', formData, {
+    params: { limit },
+    skipErrorModal: true,
+  });
+  return data.data;
+};
+
+export const fetchRecommendation = async (id) => {
+  const { data } = await instance.get(`/recommendations/${id}`, {
+    skipErrorModal: true,
+  });
+  return data.data;
+};
